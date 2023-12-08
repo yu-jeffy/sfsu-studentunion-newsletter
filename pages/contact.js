b25623f2d52d906057b4444eb1d4c9d4a794cd86
Jeff Yu <105130385+yu-jeffy@users.noreply.github.com>
2023-12-08 19:20:44 +0000

    contact links, header margin

--- pages/contact.js
+++ pages/contact.js
@@ -1,17 +1,16 @@
 import Layout from '../components/Layout';
 import styles from './contact.module.css';
+import Link from 'next/link';
 
 const Contact = () => {
   return (
     <Layout>
       <div className={styles.contact}>
         <h1>Contact Us</h1>
-        <p>If you have any questions or just want to get in touch, feel free to contact us:</p>
+        <p>If you have any questions or want to get involved, feel free to contact us:</p>
         <ul>
-          <li>Email: contact@example.com</li>
-          <li>Instagram: @ Here</li>
-          <li>Discord: Link Here</li>
-          <li>Contact: Text</li>
+          <li>Instagram: @<Link className={styles.contactLink} href="https://www.instagram.com/sfsustudentunion">sfsustudentunion</Link></li>
+          <li>Discord: <Link className={styles.contactLink} href="https://discord.gg/Xcn9JBczXB">Join Our Discord Server</Link></li>
         </ul>
       </div>
     </Layout>
