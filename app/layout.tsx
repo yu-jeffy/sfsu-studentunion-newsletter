ac734e64e35bcbbe1e1310e7d1b925d037dc0a03
Jeffy Yu <105130385+yu-jeffy@users.noreply.github.com>
2024-04-26 09:22:45 +0000

    favicon fix, vercel/github removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 export const metadata = {
-  title: 'SFSU Student Union',
-  description: 'Student Union Organization at San Francisco State University',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
+    title: 'SFSU Student Union',
+    description: 'Student Union Organization at San Francisco State University',
+  }
+  
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
